2197773e75328093da170092cb6588ae60d94d33
meg <megnew27@gmail.com>
2018-06-07 20:05:09 -0400

    Test Scenario for BabySitterPay before MidNight -- Expected to fail

---
+++ BabySitterTests/UnitTestBabySitter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BabySitterTests
+{
+    [TestClass]
+    public class UnitTestBabySitter
+    {
+        [TestMethod]
+        public void TestPayBeforeBedTime()
+        {
+            BabySitterPay babySitterPay = new BabySitterPay();
+            Double totalCharge = babySitterPay.PayBeforeBedTime(startTime, endTime);
+            Assert.AreEqual(1, totalCharge);
+
+        }
+    }
+}
