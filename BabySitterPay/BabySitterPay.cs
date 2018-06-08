a18a8a6764c9fee56ca1678c43287a62ced038c3
meg <megnew27@gmail.com>
2018-06-07 23:32:35 -0400

    Add General Method to calculate BabySitterPay Total Pay -- CalcBabySitterPay Add conditional on General Method that calls specific Method to calculate Pay based on hourly worked and rate rename Test Method --TestCalcBabySitterPay Rewrite Test Scenario -- Expected to fail as there is no implementation Yet

--- BabySitterPay/BabySitterPay.cs
+++ BabySitterPay/BabySitterPay.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,50 @@ namespace BabySitter
     public class BabySitterPay
     {
         private const Double BEDTIMERATE = 12;
-        public Double PayBeforeBedTime(int startTime, int endTime)
+        private const Double AFTERBEDTIMERATE = 8;
+        private const Double AFTERMIDNIGHTRATE = 16;
+        private const int MIDNIGHT = 6;
+
+        public Double CalcBabySitterPay(int startTime, int endTime, int bedTime)
         {
-            Double NightlyPay = 0.00;
-            NightlyPay = (endTime - startTime) * BEDTIMERATE;
-            return NightlyPay;
+                Double totalPay = 0.00;
+            try
+            {
+                if (endTime <= bedTime)
+                {
+                    totalPay = CalcPayBeforeBedTime(startTime, endTime);
+                }
+
+                if (endTime > bedTime && endTime <= MIDNIGHT)
+                {
+                    totalPay = CalcPayBeforeBedTime(startTime, bedTime) + CalcPayAfterBedTime(bedTime, endTime);
+                }
+
+                if (endTime > MIDNIGHT)
+                {
+                    totalPay = CalcPayBeforeBedTime(startTime, bedTime) + CalcPayAfterBedTime(bedTime, endTime) +
+                               CalcPayAfterMidNight(endTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex; // TO DO capture specific exception and Log 
+            }
+                return totalPay;
+        }
+        public Double CalcPayBeforeBedTime(int startTime, int endTime)
+        {
+            return 0;
+        }
+
+        public Double CalcPayAfterBedTime(int bedTime, int endTime)
+        {
+            return 0;
+        }
+
+        public Double CalcPayAfterMidNight(int endTime)
+        {
+            return 0;
         }
     }
 }
--- BabySitterTests/UnitTestBabySitter.cs
+++ BabySitterTests/UnitTestBabySitter.cs
@@ -4,14 +4,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BabySitterTests
 {
+    
     [TestClass]
     public class UnitTestBabySitter
     {
         [TestMethod]
-        public void TestPayBeforeBedTime()
+        public void TestCalcBabySitterPay()
         {
             BabySitterPay babySitterPay = new BabySitterPay();
-            Double totalCharge = babySitterPay.PayBeforeBedTime(6, 7);
+            Double totalCharge = babySitterPay.CalcBabySitterPay(6, 7, 9);
             Assert.AreEqual(12, totalCharge);
 
         }
